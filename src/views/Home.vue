e07607acfa49d3fc2af895bd1da448abfe1e1d99
Baothuctap <nhocteng6789@gmail.com>
2021-01-30 09:59:12 +0700

    tao source lan dau

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>Home Page Videos</h1>
+   
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// @ts-ignore
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data: ()=>{
+    return {
+        videos : [
+
+        ]
+    }
+  }
+}
+</script>
